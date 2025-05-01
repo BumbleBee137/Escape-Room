a2ca5873707da2f45f468994d0df7dffe15843ca
Bee Millie <bee.millie137@gmail.com>
2025-05-01 14:56:04 -0400

    Item.h created

--- src/Items/item.h
+++ src/Items/item.h
@@ -5,4 +5,25 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include "shape.h"
+#include "../shader/shader.h"
+#include <memory>
+using std::make_unique, std::unique_ptr, glm::vec2;
+
+class Item {
+private:
+    vector<unique_ptr<Shape>> shapes;
+
+public:
+    // Constructors
+    Item();
+    Item(Shader& shader, vec2 pos);
+
+    // Draw the item
+    void setUniformsAndDraw() const;
+
+    // Return true if overlaps with a point given
+    bool isOverlapping(const vect2& point) const;
+};
+
 #endif //ITEM_H
